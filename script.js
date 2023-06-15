9b3f763188d848bcd70f714927b56f801a308a6f
LTCrash <86897372+LTCrash@users.noreply.github.com>
2023-06-14 22:45:05 -0300

    Mais comentado //

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var time = new Date()
-var emini = document.getElementById('emini')
+var emini = document.getElementById('emini') //importa os inputs por id
 var pasini = document.getElementById('pasini')
 var emerro = document.getElementById('emerro')
 var abrirform = document.getElementById('abrirform')
@@ -18,8 +18,8 @@ pasini.addEventListener('focus', () => {
 
 //retorna a outline do formulario quando sai do foco
 emini.addEventListener('blur', () => {
-    if (emini.value == "") {
-        emini.style.borderColor = '#ccc'
+    if (emini.value == "") { // volta os botões para como estavam no inicio
+        emini.style.borderColor = '#ccc' // todos os styles estão comentados no staly.css
         emerro.style.display = 'none'
         botao = 'entrar'
         abrirform.style.width = '15%'
@@ -29,14 +29,14 @@ emini.addEventListener('blur', () => {
         enviarform.style.cursor = 'pointer'
         enviarform.style.background = '#600833'
         return
-    } else if (!EmailValido(emini.value)) {
+    } else if (!EmailValido(emini.value)) { // valida o email para mudar a cor da caixa
         emini.style.borderColor = '#b00'
         emerro.style.display = 'block'
         return
     } else {
-        if (emini.value !== "teste@teste.teste") {
-            botao = 'registrar'
-            abrirform.style.width = '84%'
+        if (emini.value !== "teste@teste.teste") { // essa parte tem que ser mudada para quando tiver banco de dados
+            botao = 'registrar'                    // para verificar se o email ja esta cadastrado
+            abrirform.style.width = '84%'          // caso não esteja ele muda a cor da caixa e os botões
             abrirform.style.cursor = 'pointer'
             abrirform.style.background = '#600833'
             enviarform.style.width = '15%'
@@ -45,7 +45,7 @@ emini.addEventListener('blur', () => {
             emini.style.borderColor = '#050'
             emerro.style.display = 'none'
         }
-        else {
+        else { //caso esteja ele deixa as cores padroes e o botão de entrar
             botao = 'entrar'
             abrirform.style.width = '15%'
             abrirform.style.cursor = 'default'
@@ -59,7 +59,7 @@ emini.addEventListener('blur', () => {
         return
     }
 })
-pasini.addEventListener('blur', () => {
+pasini.addEventListener('blur', () => { //blur da senha #vai ser alterado
     pasini.style.borderColor = '#ccc'
 })
 
@@ -69,33 +69,33 @@ const formlogin = document.getElementById('logini');
 //Envia alerta de erro no fomulario
 formlogin.addEventListener("submit", (event) => {
     event.preventDefault()
-    if (botao == 'registrar'){
+    if (botao == 'registrar'){ //ver se ta na tela de registro e invalida o botão submit
         return
     }
-    if (!EmailValido(emini.value) || emini.value == "") {
+    if (!EmailValido(emini.value) || emini.value == "") { // verifica se o email é invalido ou esta vazio para emitir um aviso 
         alert("Digite um email valido")
         return
     }
-    if (!senhavalida(pasini.value, 8)) {
+    if (!senhavalida(pasini.value, 8)) { // verifica se a senha é invalida para emitir um aviso 
         alert("Digite uma senha valida")
         return
     }
-    formlogin.submit()
+    formlogin.submit() // envia o formulario
 })
 
 //Valida o email
 function EmailValido(email) {
     const rejeitar = new RegExp(
-        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/ //formata o email
     )
-    if(rejeitar.test(email)) {
+    if(rejeitar.test(email)) { // testa o emais de acordo com a formatação
         return true
     }
     return false
 }
 
 //Valida Senha
-function senhavalida(senha, minimo) {
+function senhavalida(senha, minimo) { // valida a senha de acordo com o tamanho
     if (senha.length >= minimo) {
         return true
     }
