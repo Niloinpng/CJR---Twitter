25650a18c2afdc313920dc3dff0eae0ec2345664
Leandro Kornelius <leobelko@gmail.com>
2023-06-22 12:22:02 -0300

    fix:arrumando problemas locais de git

--- back-end/prisma/migrations/20230619164635_add_nucleo_unico/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[nome]` on the table `Nucleo` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Nucleo_nome_key" ON "Nucleo"("nome");
