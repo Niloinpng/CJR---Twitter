ac5c86b67ce39d3dfa4e6a2940350bc86bf4087f
Leandro Kornelius <leobelko@gmail.com>
2023-06-23 11:47:57 -0300

    fix:nova tentativa de conectar o front com o back

--- teste.js
+++ teste.js
@@ -1,3 +1,84 @@
+const form = () => {
+  const elements = {
+    emailInput: document.getElementById("email-input"),
+    senhaInput: document.getElementById("senha-input"),
+    nomeInput: document.getElementById("nome-input"),
+    generoInput: document.getElementById("genero-input"),
+    cargoInput: document.getElementById("cargo-input"),
+    nucleoInput: document.getElementById("nucleo-input"),
+    rootForm: document.querySelector("form"),
+    btn: document.querySelector("button"),
+  };
+
+  let state = {
+    emailInput: "", 
+    senhaInput: "",
+    nomeInput: "",
+    generoInput: "",
+    cargoInput: "",
+    nucleoInput: "",
+  }
+
+  let setEventHandlers = () => {
+
+    elements.emailInput.addEventListener("change", () => {
+      console.log(elements.emailInput.value);
+      state.emailInput = elements.emailInput.value;
+    });
+
+    elements.senhaInput.addEventListener("change", () => {
+      console.log(elements.senhaInput.value);
+      state.senhaInput = elements.senhaInput.value;
+    });
+
+    elements.nomeInput.addEventListener("change", () => {
+      console.log(elements.nomeInput.value);
+      state.nomeInput = elements.nomeInput.value;
+    });
+
+    elements.generoInput.addEventListener("change", () => {
+      console.log(elements.generoInput.value);
+      state.generoInput = elements.generoInput.value;
+    });
+
+    elements.cargoInput.addEventListener("change", () => {
+      console.log(elements.cargoInput.value);
+      state.cargoInput = elements.cargoInput.value;
+    });
+
+    elements.nucleoInput.addEventListener("change", () => {
+      console.log(elements.nucleoInput.value);
+      state.nucleoInput = elements.nucleoInput.value;
+    });
+
+    elements.rootForm.addEventListener("submit", async (e) => {
+      e.preventDefault();
+
+      const body = {
+        input1: state.emailInput,
+        input2: state.senhaInput,
+        input3: state.nomeInput,
+        input4: state.generoInput,
+        input5: state.cargoInput,
+        input6: state.nucleoInput,
+      };
+
+      console.log(body);
+
+      const res = await fetch("/back-end/autentificacao.cntroll.js", {
+        method: "post", 
+        headers: {},
+        body: JSON.stringify(body),
+      });
+    });
+  };
+
+  return setEventHandlers;
+};
+
+form().setEventHandlers();
+
+/*
 import { getEnvironmentData } from "worker_threads";
 
 clickLogin = (e) => {
@@ -23,3 +104,4 @@ clickLogin = (e) => {
        }
     });
   }
+*/
