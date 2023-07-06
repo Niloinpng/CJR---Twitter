c0d78b481c297cc0ff6b0320009f9ea6f5d196d6
Leandro Kornelius <leobelko@gmail.com>
2023-07-05 22:32:58 -0300

    feat:campo de publicar e comentar no formato de modal

--- feed.js
+++ feed.js
@@ -1,18 +1,11 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let publishButton = document.getElementById("publicar");
-    let publishContent = document.getElementById("content");
-    let publishCancel = document.getElementById("cancelar");
-    let commentButton = document.getElementById("comment-button");
+function abreModal(event) {
+    event.preventDefault();
+    let publishContent = document.querySelector(".modal-overlay");
+    publishContent.classList.add("modal-visible")
+};
 
-    publishButton.addEventListener("click", () => {
-        publishContent.style.display = "block";
-    });
-
-    commentButton.addEventListener("click", () => {
-        publishContent.style.display = "block";
-    });
-
-    publishCancel.addEventListener("click", () => {
-        publishContent.style.display = "none";
-    });
-});
+function fechaModal(event) {
+    event.preventDefault();
+    let publishContent = document.querySelector(".modal-overlay");
+    publishContent.classList.remove("modal-visible")
+};
