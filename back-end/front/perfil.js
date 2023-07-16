7c46485239bc6b88e99a9e7c002b09ca01baf022
Nilo Silva <danilo.silveira.silv@gmail.com>
2023-07-16 13:28:05 -0300

    push: algumas mudanças bobas

--- back-end/front/perfil.js
+++ back-end/front/perfil.js
@@ -41,3 +41,15 @@ async function padrao (id){
 }
 
 padrao(19)
+
+function abreModal(event) {
+    event.preventDefault();
+    let publishContent = document.querySelector(".modal-overlay");
+    publishContent.classList.add("modal-visible")
+};
+
+function fechaModal(event) {
+    event.preventDefault();
+    let publishContent = document.querySelector(".modal-overlay");
+    publishContent.classList.remove("modal-visible")
+};
