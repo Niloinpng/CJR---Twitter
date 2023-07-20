a8fbbf0d373d825eec6f7540ae3fe5031478bed8
Leandro Kornelius <leobelko@gmail.com>
2023-07-19 21:46:59 -0300

    feat:adiciona markdown no html

--- back-end/front/feed.js
+++ back-end/front/feed.js
@@ -9,3 +9,10 @@ function fechaModal(event) {
     let publishContent = document.querySelector(".modal-overlay");
     publishContent.classList.remove("modal-visible")
 };
+
+// Most options demonstrate the non-default behavior
+var simplemde = new SimpleMDE({
+	element: document.getElementById("md"),
+	placeholder: "Converse com a gente...",
+    toolbar: ["bold", "italic", "heading", "|", "quote"]
+});
